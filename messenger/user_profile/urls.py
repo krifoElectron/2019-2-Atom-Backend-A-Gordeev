c470e7ed3bbd5200c12e6e534d80fed3304d92f1
krifoElectron <ars2d2@yandex.ru>
2019-12-16 23:25:03 +0300

    hw_06

--- messenger/user_profile/urls.py
+++ messenger/user_profile/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from user_profile.views import profile, contacts
+from user_profile.views import profile, contacts, search_user, get_user_by_any
 
 urlpatterns = [
     path('profile/', profile, name='profile'),
     path('contacts/', contacts, name='contacts'),
+    path('search_user', search_user, name='search_user'),
+    path('get_user_by_any', get_user_by_any, name='get_user_by_any')
 ]
